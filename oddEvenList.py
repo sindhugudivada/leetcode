0192812439a368026bd7018e84e9673b3122fd1b
Sindhu <sindhugudivada67@gmail.com>
2019-07-26 23:06:53 -0700

    Create oddEvenList.py

---
+++ oddEvenList.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def oddEvenList(self, head: ListNode) -> ListNode:
+        if not head:
+            return 
+        odd = head
+        even = head.next
+        
+        evenHead = even
+        while even and even.next:
+            odd.next = odd.next.next
+            even.next = even.next.next
+            odd = odd.next
+            even = even.next
+        odd.next = evenHead
+        return head
+            
+        
+        
